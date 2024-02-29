444521a63b2e12ccf5093f9751670b582ee8b228
wangqingzheng <wangqingzheng@kylinos.cn>
2024-02-29 16:10:23 +0800

    Add function create lmbench data

---
+++ lmbench/views.py
@@ -0,0 +1,125 @@
+# Create your views here.
+from rest_framework import status
+from appStore.lmbench.serializers import LmbenchSerializer
+from appStore.utils.common import LimsPageSet, json_response, get_error_message, return_time
+from appStore.utils.customer_view import CusModelViewSet
+
+
+class LmbenchViewSet(CusModelViewSet):
+    """
+    Lmbench数据管理
+    """
+    # queryset = Stream.objects.all().order_by('id')
+    serializer_class = LmbenchSerializer
+
+    # pagination_class = LimsPageSet
+
+    def create(self, request, *args, **kwargs):
+        serializer_error = []
+        error_message = []
+        for k, lmbench_json in request.__dict__['data_lmbench'].items():
+            if k.lower().startswith('lmbench'):
+                for unixbench_json in lmbench_json['items']:
+                    # 每一条lmbench数据
+                    lmbench = {}
+                    lmbench['env_id'] = request.__dict__['data_lmbench']['env_id']
+                    lmbench['test_time'] = return_time(lmbench_json['time'])
+                    lmbench['execute_cmd'] = 'xxx'
+                    lmbench['modify_parameters'] = 'xxx'
+                    # 处理数据，原始数据每个一级标就是一个json，把所有以及字段放到一个json中
+                    new_lmbench = {}
+                    for i in unixbench_json:
+                        new_lmbench.update(i)
+                    for key, value in new_lmbench.items():
+                        if key == "Basic system parameters":
+                            lmbench['basic_Mhz'] = value['Mhz']
+                            lmbench['basic_tlb_pages'] = value['tlb pages']
+                            lmbench['basic_cache_line_bytes'] = value['cache line bytes']
+                            lmbench['basic_mem_par'] = value['mem par']
+                            lmbench['basic_scal_load'] = value['scal load']
+                        elif key == "Processor":
+                            lmbench['processor_null_call'] = value['Mhz']
+                            lmbench['processor_null_I_O'] = value['null call']
+                            lmbench['processor_stat'] = value['null I/O']
+                            lmbench['processor_open_clo'] = value['stat']
+                            lmbench['processor_slct_TCP'] = value['open close']
+                            lmbench['processor_sig_inst'] = value['slct TCP']
+                            lmbench['processor_sig_hndl'] = value['sig inst']
+                            lmbench['processor_fork_proc'] = value['sig hndl']
+                            lmbench['processor_exec_proc'] = value['fork proc']
+                            lmbench['processor_sh_proc'] = value['exec proc']
+                            lmbench['processor_Mhz'] = value['sh proc']
+                        elif key == "Basic integer operations":
+                            lmbench['basic_intgr_bit'] = value['intgr bit']
+                            lmbench['basic_intgr_add'] = value['intgr add']
+                            lmbench['basic_intgr_mul'] = value['intgr mul']
+                            lmbench['basic_intgr_div'] = value['intgr div']
+                            lmbench['basic_intgr_mod'] = value['intgr mod']
+                        elif key == "Basic uint64 operations":
+                            lmbench['basic_int64_bit'] = value['int64 bit']
+                            lmbench['basic_int64_add'] = value['int64 add']
+                            lmbench['basic_int64_mul'] = value['int64 mul']
+                            lmbench['basic_int64_div'] = value['int64 div']
+                            lmbench['basic_int64_mod'] = value['int64 mod']
+                        elif key == "Basic float operations":
+                            lmbench['basic_float_add'] = value['float add']
+                            lmbench['basic_float_mul'] = value['float mul']
+                            lmbench['basic_float_div'] = value['float div']
+                            lmbench['basic_float_bogo'] = value['float bogo']
+                        elif key == "Basic double operations":
+                            lmbench['basic_double_add'] = value['double add']
+                            lmbench['basic_double_mul'] = value['double mul']
+                            lmbench['basic_double_div'] = value['double div']
+                            lmbench['basic_double_bogo'] = value['double bogo']
+                        elif key == "Context switching":
+                            lmbench['context_2p_0K'] = value['2p/0K']
+                            lmbench['context_2p_16K'] = value['2p/16K']
+                            lmbench['context_2p_64K'] = value['2p/64K']
+                            lmbench['context_8p_16K'] = value['8p/16K']
+                            lmbench['context_8p_64K'] = value['8p/64K']
+                            lmbench['context_16p_16K'] = value['16p/16K']
+                            lmbench['context_16p_64K'] = value['16p/64K']
+                        elif key == "*Local* Communication latencies":
+                            lmbench['local_2p_0K'] = value['2p/0K']
+                            lmbench['local_Pipe'] = value['Pipe']
+                            lmbench['local_AF_UNIX'] = value['AF UNIX']
+                            lmbench['local_UDP'] = value['UDP']
+                            lmbench['local_TCP'] = value['TCP']
+                            lmbench['local_TCP_conn'] = value['TCP conn']
+                            lmbench['local_RPC_TCP'] = value['RPC/TCP']
+                            lmbench['local_RPC_UDP'] = value['RPC/UDP']
+                        elif key == "File & VM system latencies in microseconds":
+                            lmbench['local_bigger_0K_File_create'] = value['0K File create']
+                            lmbench['local_bigger_0K_File_delete'] = value['0K File delete']
+                            lmbench['local_bigger_10K_File_create'] = value['10K File create']
+                            lmbench['local_bigger_10K_File_delete'] = value['10K File delete']
+                            lmbench['local_bigger_Mmap_Latency'] = value['Mmap Latency']
+                            lmbench['local_bigger_Prot_Fault'] = value['Prot Fault']
+                            lmbench['local_bigger_Page_Fault'] = value['Page Fault']
+                            lmbench['local_bigger_100fd_selct'] = value['100fd selct']
+                        elif key == "*Local* Communication bandwidths in MB/s - bigger is better":
+                            lmbench['local_bigger_Pipe'] = value['Pipe']
+                            lmbench['local_bigger_AF_UNIX'] = value['AF UNIX']
+                            lmbench['local_bigger_TCP'] = value['TCP']
+                            lmbench['local_bigger_File_reread'] = value['File reread']
+                            lmbench['local_bigger_Mmap_reread'] = value['Mmap reread']
+                            lmbench['local_bigger_Bcopy_libc'] = value['Bcopy(libc)']
+                            lmbench['local_bigger_Bcopy_hand'] = value['Bcopy(hand)']
+                            lmbench['local_bigger_Mem_read'] = value['Mem read']
+                            lmbench['local_bigger_Mem_write'] = value['Mem write']
+                        elif key == "Memory latencies in nanoseconds":
+                            lmbench['memory_Mhz'] = value['Mhz']
+                            lmbench['memory_L1'] = value['L1 $']
+                            lmbench['memory_L2'] = value['L2 $']
+                            lmbench['memory_Main_mem'] = value['Main mem']
+                            lmbench['memory_Rand_mem'] = value['Rand mem']
+                    serializer_lmbench = LmbenchSerializer(data=lmbench)
+                    if serializer_lmbench.is_valid():
+                        # todo 放开
+                        pass
+                        # self.perform_create(serializer_lmbench)
+                    serializer_error.append(serializer_lmbench.errors)
+                    error_message.append(get_error_message(serializer_lmbench))
+        if serializer_error:
+            print(serializer_error,"lmbench")
+            return json_response(serializer_error, status.HTTP_400_BAD_REQUEST, error_message)
