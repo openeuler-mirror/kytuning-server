0ec42dc7b7a25b04f77c4135048ac0dc1e11b3d3
wangqingzheng <wangqingzheng@kylinos.cn>
2024-03-05 13:37:33 +0800

    Modify the unixbench database and modify the corresponding creation method

--- appStore/unixbench/views.py
+++ appStore/unixbench/views.py
@@ -16,51 +16,39 @@ class UnixbenchViewSet(CusModelViewSet):
     # pagination_class = LimsPageSet
 
     def create(self, request, *args, **kwargs):
+        serializer_unixbench_errors = []
+        error_message = []
         data_unixbench = {}
         for k, unixbench_json in request.__dict__['data_unixbench'].items():
             if k.lower().startswith('unixbench'):
                 data_unixbench['env_id'] = request.__dict__['data_unixbench']['env_id']
+                thread = "多线程"
                 if k.split('-')[-3] == 'single':
-                    # todo 所有的参数 、 cmd 是在哪里保存的
-                    data_unixbench['single_thread'] = '单线程'
-                    data_unixbench['single_execute_cmd'] = 'xx'
-                    data_unixbench['single_modify_parameters'] = '参数'
-                    data_unixbench['single_Dhrystone'] = unixbench_json['单线程']['Dhrystone 2 using register variables(lps)']
-                    data_unixbench['single_Double_Precision'] = unixbench_json['单线程']['Double-Precision Whetstone(MWIPS)']
-                    data_unixbench['single_execl_throughput'] = unixbench_json['单线程']['Execl Throughput(lps)']
-                    data_unixbench['single_file_copy_1024'] = unixbench_json['单线程']['File Copy 1024 bufsize 2000 maxblocks(KBps)']
-                    data_unixbench['single_file_copy_256'] = unixbench_json['单线程']['File Copy 256 bufsize 500 maxblocks(KBps)']
-                    data_unixbench['single_file_copy_4096'] = unixbench_json['单线程']['File Copy 4096 bufsize 8000 maxblocks(KBps)']
-                    data_unixbench['single_pipe_throughput'] = unixbench_json['单线程']['Pipe Throughput(lps)']
-                    data_unixbench['single_pipe_based'] = unixbench_json['单线程']['Pipe-based Context Switching(lps)']
-                    data_unixbench['single_process_creation'] = unixbench_json['单线程']['Process Creation(lps)']
-                    data_unixbench['single_shell_scripts_1'] = unixbench_json['单线程']['Shell Scripts (1 concurrent)(lpm)']
-                    data_unixbench['single_shell_scripts_8'] = unixbench_json['单线程']['Shell Scripts (8 concurrent)(lpm)']
-                    data_unixbench['single_system_call_overhead'] = unixbench_json['单线程']['System Call Overhead(lps)']
-                    data_unixbench['single_index_score'] = unixbench_json['单线程']['Index Score(sum)']
-                    data_unixbench['single_test_time'] = return_time(unixbench_json['time'])
-                elif  k.split('-')[-3] == 'multi':
-                    data_unixbench['multi_thread'] = '多线程'
-                    data_unixbench['multi_execute_cmd'] = 'xx'
-                    data_unixbench['multi_modify_parameters'] = '参数'
-                    data_unixbench['multi_Dhrystone'] = unixbench_json['多线程']['Dhrystone 2 using register variables(lps)']
-                    data_unixbench['multi_Double_Precision'] = unixbench_json['多线程']['Double-Precision Whetstone(MWIPS)']
-                    data_unixbench['multi_execl_throughput'] = unixbench_json['多线程']['Execl Throughput(lps)']
-                    data_unixbench['multi_file_copy_1024'] = unixbench_json['多线程']['File Copy 1024 bufsize 2000 maxblocks(KBps)']
-                    data_unixbench['multi_file_copy_256'] = unixbench_json['多线程']['File Copy 256 bufsize 500 maxblocks(KBps)']
-                    data_unixbench['multi_file_copy_4096'] = unixbench_json['多线程']['File Copy 4096 bufsize 8000 maxblocks(KBps)']
-                    data_unixbench['multi_pipe_throughput'] = unixbench_json['多线程']['Pipe Throughput(lps)']
-                    data_unixbench['multi_pipe_based'] = unixbench_json['多线程']['Pipe-based Context Switching(lps)']
-                    data_unixbench['multi_process_creation'] = unixbench_json['多线程']['Process Creation(lps)']
-                    data_unixbench['multi_shell_scripts_1'] = unixbench_json['多线程']['Shell Scripts (1 concurrent)(lpm)']
-                    data_unixbench['multi_shell_scripts_8'] = unixbench_json['多线程']['Shell Scripts (8 concurrent)(lpm)']
-                    data_unixbench['multi_system_call_overhead'] = unixbench_json['多线程']['System Call Overhead(lps)']
-                    data_unixbench['multi_index_score'] = unixbench_json['多线程']['Index Score(sum)']
-                    data_unixbench['multi_test_time'] = return_time(unixbench_json['time'])
-        serializer_unixbench = UnixbenchSerializer(data=data_unixbench)
-        if serializer_unixbench.is_valid():
-            pass
-            # todo 放开
-            # self.perform_create(serializer_unixbench)
-        return json_response(serializer_unixbench.errors, status.HTTP_400_BAD_REQUEST,
-                             get_error_message(serializer_unixbench))
+                    thread = "单线程"
+                # todo 所有的参数 、 cmd 是在哪里保存的
+                data_unixbench['thread'] = thread
+                data_unixbench['execute_cmd'] = 'xx'
+                data_unixbench['modify_parameters'] = '参数'
+                print(unixbench_json,111)
+                data_unixbench['Dhrystone'] = unixbench_json[thread]['Dhrystone 2 using register variables(lps)']
+                data_unixbench['Double_Precision'] = unixbench_json[thread]['Double-Precision Whetstone(MWIPS)']
+                data_unixbench['execl_throughput'] = unixbench_json[thread]['Execl Throughput(lps)']
+                data_unixbench['file_copy_1024'] = unixbench_json[thread]['File Copy 1024 bufsize 2000 maxblocks(KBps)']
+                data_unixbench['file_copy_256'] = unixbench_json[thread]['File Copy 256 bufsize 500 maxblocks(KBps)']
+                data_unixbench['file_copy_4096'] = unixbench_json[thread]['File Copy 4096 bufsize 8000 maxblocks(KBps)']
+                data_unixbench['pipe_throughput'] = unixbench_json[thread]['Pipe Throughput(lps)']
+                data_unixbench['pipe_based'] = unixbench_json[thread]['Pipe-based Context Switching(lps)']
+                data_unixbench['process_creation'] = unixbench_json[thread]['Process Creation(lps)']
+                data_unixbench['shell_scripts_1'] = unixbench_json[thread]['Shell Scripts (1 concurrent)(lpm)']
+                data_unixbench['shell_scripts_8'] = unixbench_json[thread]['Shell Scripts (8 concurrent)(lpm)']
+                data_unixbench['system_call_overhead'] = unixbench_json[thread]['System Call Overhead(lps)']
+                data_unixbench['index_score'] = unixbench_json[thread]['Index Score(sum)']
+                data_unixbench['test_time'] = return_time(unixbench_json['time'])
+                serializer_unixbench = UnixbenchSerializer(data=data_unixbench)
+                if serializer_unixbench.is_valid():
+                    pass
+                    # todo 放开
+                    # self.perform_create(serializer_unixbench)
+                serializer_unixbench_errors.append(serializer_unixbench.errors)
+                error_message.append(get_error_message(serializer_unixbench))
+        return json_response(serializer_unixbench_errors, status.HTTP_400_BAD_REQUEST, error_message)
