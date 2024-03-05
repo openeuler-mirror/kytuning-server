872c9628be5a073c5c40f2f1d43ac109cfab01e6
wangqingzheng <wangqingzheng@kylinos.cn>
2024-03-05 13:43:12 +0800

    add web show project and env

--- appStore/api/router.py
+++ appStore/api/router.py
@@ -2,14 +2,18 @@ from django.conf.urls import url, include
 from rest_framework import routers
 
 from appStore.env.views import EnvViewSet
+from appStore.project.views import ProjectViewSet
+
 
 router = routers.DefaultRouter()
 ####################
 #   公共地址映射  #
 ###################
 router.register(r'env', EnvViewSet, basename='env')
+router.register(r'project', ProjectViewSet, basename='project')
 
 urlpatterns = [
     # url(r'^y_upload_file/', y_upload_file),  # upload_file
     url(r'', include(router.urls)),
+    router.register(r'project', ProjectViewSet, basename='project')
 ]
