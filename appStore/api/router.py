af4e65214da0b1353a01ccbb2f01b21e7e7813c6
wangqingzheng <wangqingzheng@kylinos.cn>
2024-03-08 09:30:26 +0800

    Add stream backend data query function

--- appStore/api/router.py
+++ appStore/api/router.py
@@ -3,12 +3,14 @@ from rest_framework import routers
 
 from appStore.env.views import EnvViewSet
 from appStore.project.views import ProjectViewSet
+from appStore.stream.views import StreamViewSet
 
 
 router = routers.DefaultRouter()
 ####################
 #   公共地址映射  #
 ###################
+router.register(r'stream', StreamViewSet, basename='stream')
 router.register(r'env', EnvViewSet, basename='env')
 router.register(r'project', ProjectViewSet, basename='project')
 
