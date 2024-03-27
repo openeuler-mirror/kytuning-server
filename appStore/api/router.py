02269a1f32a6ee4830a955b45a98ee216155f712
wqz <wangqingzheng@kylinos.cn>
2024-03-27 14:51:46 +0800

    Add URL for benchmark performance testing

--- appStore/api/router.py
+++ appStore/api/router.py
@@ -1,21 +1,33 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
+from appStore.cpu2006.views import Cpu2006ViewSet
+from appStore.cpu2017.views import Cpu2017ViewSet
 from appStore.env.views import EnvViewSet
+from appStore.fio.views import FioViewSet
+from appStore.iozone.views import IozoneViewSet
+from appStore.jvm2008.views import Jvm2008ViewSet
+from appStore.lmbench.views import LmbenchViewSet
 from appStore.project.views import ProjectViewSet
 from appStore.stream.views import StreamViewSet
-
+from appStore.unixbench.views import UnixbenchViewSet
 
 router = routers.DefaultRouter()
 ####################
 #   公共地址映射  #
 ###################
-router.register(r'stream', StreamViewSet, basename='stream')
+router.register(r'cpu2006', Cpu2006ViewSet, basename='cpu2006')
+router.register(r'cpu2017', Cpu2017ViewSet, basename='cpu2017')
 router.register(r'env', EnvViewSet, basename='env')
+router.register(r'fio', FioViewSet, basename='fio')
+router.register(r'iozone', IozoneViewSet, basename='iozone')
+router.register(r'jvm2008', Jvm2008ViewSet, basename='jvm2008')
+router.register(r'lmbench', LmbenchViewSet, basename='lmbench')
 router.register(r'project', ProjectViewSet, basename='project')
+router.register(r'stream', StreamViewSet, basename='stream')
+router.register(r'unixbench', UnixbenchViewSet, basename='unixbench')
 
 urlpatterns = [
     # url(r'^y_upload_file/', y_upload_file),  # upload_file
     url(r'', include(router.urls)),
-    router.register(r'project', ProjectViewSet, basename='project')
 ]
