609d77109758c439ea779a9abff3e6553ba8149d
wqz <wangqingzheng@kylinos.cn>
2024-04-17 15:29:36 +0800

    Add stream data display function

--- appStore/stream/views.py
+++ appStore/stream/views.py
@@ -1,7 +1,14 @@
+import json
+
+from django.http import JsonResponse, request
+from django.shortcuts import render
+
 # Create your views here.
 from rest_framework import status
+
 from appStore.stream.models import Stream
 from appStore.stream.serializers import StreamSerializer
+from appStore.utils import constants
 from appStore.utils.common import LimsPageSet, json_response, get_error_message, return_time
 from appStore.utils.customer_view import CusModelViewSet
 
@@ -13,11 +20,29 @@ class StreamViewSet(CusModelViewSet):
     queryset = Stream.objects.all().order_by('id')
     serializer_class = StreamSerializer
 
+    pagination_class = LimsPageSet
+
+    def list(self, request, *args, **kwargs):
+        """
+        返回列表
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        env_id = request.GET.get('env_id')
+        queryset = Stream.objects.filter(env_id=env_id).all()
+        queryset = self.filter_queryset(queryset)
+        serializer = self.get_serializer(queryset, many=True)
+        return json_response(serializer.data, status.HTTP_200_OK, '列表')
+
+
     def create(self, request, *args, **kwargs):
         serializer_stream_errors = []
         error_message = []
         for k, stream_json in request.__dict__['data_stream'].items():
             if k.lower().startswith('stream'):
+                constants.STREAM_BOOL = True
                 data_stream = {}
                 data_stream['env_id'] = request.__dict__['data_stream']['env_id']
                 # todo 所有的参数 、 cmd 是在哪里保存的
@@ -38,10 +63,8 @@ class StreamViewSet(CusModelViewSet):
                 data_stream['test_time'] = return_time(stream_json['time'])
                 serializer_stream = StreamSerializer(data=data_stream)
                 if serializer_stream.is_valid():
-                    pass
-                    # todo 放开
                     # self.perform_create(serializer_stream)
-                print(serializer_stream.errors,"stream")
+                    pass
                 serializer_stream_errors.append(serializer_stream.errors)
                 error_message.append(get_error_message(serializer_stream))
 
