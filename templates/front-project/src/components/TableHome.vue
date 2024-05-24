7a4f0c40228de778b2d0e138c3f996edf17b3b3f
wqz <wangqingzheng@kylinos.cn>
2024-05-24 09:29:14 +0800

    Update the data display method in the home component

--- templates/front-project/src/components/TableHome.vue
+++ templates/front-project/src/components/TableHome.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="tableHome">
-    <template v-if="isDataLoaded">
-      <Header :dataName="dataName" baseId="baseId" comparsionIds="comparsionIds" ref="headerComponent" @data-loaded="accessChildData"/>
-      <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="accessChildData"/>
-<!--      <el-container class="content">-->
-<!--        <el-container>-->
-<!--          <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="accessChildData"/>-->
-<!--          <Base :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="baseComponent" @data-loaded="accessChildData"/>-->
-<!--          <Compar :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="comparComponent" @data-loaded="accessChildData"/>-->
-<!--        </el-container>-->
-<!--      </el-container>-->
+    <template v-if="isAllDataLoaded">
+      <Header :dataName="dataName" baseId="baseId" comparsionIds="comparsionIds" ref="headerComponent" @data-loaded="getHeadrData"/>
+<!--      <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="accessChildData"/>-->
+      <el-container class="content">
+        <el-container>
+          <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="getLeftData"/>
+          <Base :dataName="dataName" :baseId="baseId" comparsionIds="comparsionIds" ref="baseComponent" @data-loaded="getBaseData"/>
+          <div v-for="comparsionId in comparsionIds" :key="comparsionId">
+<!--            <Compar :dataName="dataName" :baseId="comparsionId" comparsionIds="comparsionIds" ref="comparComponent" @data-loaded="accessChildData"/>-->
+            <!--           传递base的数据平均值，用于计算上升还是下降 -->
+            <template v-if="isComparDataLoaded">
+              <Compar :dataName="dataName" :baseId="comparsionId" :titleIndex="titleIndex" ref="comparComponent"
+                      v-model="comparDatas[comparsionId]" @data-loaded="getComparData"/>
+            </template>
+          </div>
+        </el-container>
+      </el-container>
       <Footer :otherList="otherList" :tableDatas="tableDatas" :dataName="dataName"/>
     </template>
   </div>
@@ -18,63 +25,90 @@
 <script>
 import Header from "./TableHeader.vue";
 import Left from "./TableLeft.vue";
-// import Base from "./BaseData.vue";
-// import Compar from "./ComparData.vue";
+import Base from "./BaseData.vue";
+import Compar from "./ComparData.vue";
 import Footer from "./TableFooter.vue";
+
 export default {
   components: {
     Header,
     Left,
-    // Base,
-    // Compar,
+    Base,
+    Compar,
     Footer
   },
   data() {
     return {
       dataName: this.$route.name,
       baseId: this.$route.params.baseId,
-      comparsionIds: this.$route.params.comparsionIds,
+      titleIndex: 1,
+      comparsionIds: this.$route.params.comparsionIds.split(","),
 
       otherList: [],
+      tableTitle: [],
       tableDatas: [],
-      baseDatas: [],
+      // baseDatas:[],
+      comparDatas: {}, // 初始化 comparDatas 对象
 
-      isDataLoaded: false, // 标记数据是否加载完成
+      isAllDataLoaded: false, // 标记数据是否加载完成
+      isComparDataLoaded: false, // 标记上一个compar数据是否加载完成
     };
   },
   created() {
-    this.isDataLoaded = true;
+    this.isAllDataLoaded = true;
+    this.isComparDataLoaded = true;
+    // this.$emit('compar-data-updated', this.comparDatas); // 发送初始 comparDatas
   },
   methods: {
-    accessChildData() {
-      const headerComponent = this.$refs.headerComponent; // 获取 Header 子组件的引用
-      if (headerComponent) {
-        this.otherList = headerComponent.otherList; // 访问 Header 子组件的 otherList 数据
-        // 在这里处理子组件的数据
-      }
+    getHeadrData(value) {
+      // const headerComponent = this.$refs.headerComponent; // 获取 Header 子组件的引用
+      // this.otherList = headerComponent.otherList; // 访问 Header 子组件的 otherList 数据
 
-      const leftComponent = this.$refs.leftComponent; // 获取 Left 子组件的引用
-      if (leftComponent) {
-        this.tableDatas = leftComponent.tableDatas; // 访问 Left 子组件的 tableDatas 数据
-        // 在这里处理子组件的数据
-      }
+      this.otherList = value; // 访问 Header 子组件的 otherList 数据
+      // 在这里处理子组件的数据
 
-      const baseComponent = this.$refs.leftComponent; // 获取 Left 子组件的引用
-      if (baseComponent) {
-        this.baseDatas = baseComponent.tableDatas; // 访问 Left 子组件的 tableDatas 数据
-        // 在这里处理子组件的数据
-      }
+    },
+    getLeftData(value) {
+      this.tableTitle = value; // 访问 Left 子组件的 tableTitle 数据
+      // 在这里处理子组件的数据
+    },
+    getBaseData(index, value) {
+      this.titleIndex = index - 1
+      console.log(this.titleIndex, 888);
+      this.tableDatas = value; // 访问 base 子组件的 tableDatas 数据
+      // console.log(this.tableDatas, 333);
+      // 在这里处理子组件的数据
+
+      // const comparComponents = this.$refs.comparComponent; // 获取 base 子组件的引用
+      // if (Array.isArray(comparComponents)) {
+      //   // 遍历 comparComponents 数组获取每个 Compar 组件的数据
+      //   comparComponents.forEach((comparComponent, index) => {
+      //     const comparsionId = this.comparsionIds[index];
+      //     if (comparComponent) {
+      //       this.comparDatas[comparsionId] = comparComponent.comparDatas; // 访问 Compar 子组件的 comparDatas 数据
+      //       console.log(comparComponent.comparDatas, 444);
+      //     }
+      //   });
+      // }
 
       // const baseDatas = [
       //   {stream1: '1', stream2: '2'},
       //   {'stream1-1': '1-1', 'stream2-2': '2-2'}
       // ];
-      // // 遍历列表 tableDatas，并将其属性合并到列表 baseDatas(目前没有baseDatas数据，模拟一下) 中对应位置的对象,后期还要加入compar数据。
-      // baseDatas.forEach((bObj, index) => {
+      // 遍历列表 tableDatas，并将其属性合并到列表 baseDatas(目前没有baseDatas数据，模拟一下) 中对应位置的对象,后期还要加入compar数据。
+      // this.baseDatas.forEach((bObj, index) => {
       //   const aObj = this.tableDatas[index];
       //   Object.assign(aObj, bObj);
       // });
-
+    },
+    getComparData(baseId, titleIndex_, comparDatas) {
+      // const comparsionId = this.comparsionIds[index];
+      // this.titleIndex = titleIndex_
+      console.log(baseId, 111);
+      console.log(titleIndex_, 222);
+      console.log(comparDatas, 333);
+      this.comparDatas[baseId] = comparDatas;
+      console.log(this.comparDatas, 444);
     },
   }
 };
@@ -82,14 +116,4 @@ export default {
 
 <style>
 @import url("//unpkg.com/element-ui@2.15.13/lib/theme-chalk/index.css");
-
-button {
-  border: none;
-  padding: 15px 30px;
-  font-size: 16px;
-  font-family: Arial, 微软雅黑;
-  color: white;
-  background: #447aa8;
-  border-radius: 3px;
-}
 </style>
