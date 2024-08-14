26762ef0ae1ff241f41afc9a595bffa8485cd626
wqz <wangqingzheng@kylinos.cn>
2024-08-14 17:31:35 +0800

    Determine whether secondary pages such as stream can perform redirection

--- templates/front-project/src/components/TableHeader.vue
+++ templates/front-project/src/components/TableHeader.vue
@@ -1,113 +1,118 @@
 <template>
-  <div class="tableHome">
-    <template v-if="isAllDataLoaded">
-<!--      <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="accessChildData"/>-->
-      <el-container class="content">
-        <el-container>
-          <Left :dataName="dataName" :baseId="baseId" :comparsionIds="comparsionIds" ref="leftComponent" @data-loaded="getLeftData"/>
-          <Base :dataName="dataName" :baseId="baseId" comparsionIds="comparsionIds" ref="baseComponent" @data-loaded="getBaseData"/>
-          <div v-for="(comparsionId,index) in comparsionIds" :key="comparsionId">{{index}}
-<!--            <Compar :dataName="dataName" :baseId="comparsionId" comparsionIds="comparsionIds" ref="comparComponent" @data-loaded="accessChildData"/>-->
-            <!--           传递base的数据平均值，用于计算上升还是下降 -->
-            <template v-if="isComparDataLoaded[index]">
-              <Compar :dataName="dataName" :baseId="comparsionId" :titleIndex="titleIndex" ref="comparComponent"
-                      v-model="comparDatas[comparsionId]" @data-loaded="getComparData"/>
-            </template>
-          </div>
-        </el-container>
-      </el-container>
-      <Footer :otherList="otherList" :tableDatas="tableDatas" :dataName="dataName"/>
-    </template>
+  <div class="footer">
+    <el-card>
+      <el-row class="mb-4">
+        <el-button type="primary" @click="exportTableData">导出表格数据</el-button>
+        <el-button type="primary" @click="toggleDataVisibility">
+          {{ showAllData ? '隐藏数据' : '显示全部数据' }}
+        </el-button>
+        <el-button type="primary" @click="$router.back()">返回</el-button>
+      </el-row>
+      <br>
+      <el-row class="mb-4">
+        <el-button type="success" @click="goToStream" :disabled="toStream">stream</el-button>
+        <el-button type="success" @click="goToLmbench">lmbench</el-button>
+        <el-button type="success" @click="goToUnixbench">unixbench</el-button>
+        <el-button type="success" @click="goToFio">fio</el-button>
+        <el-button type="success" @click="goToIozone">iozone</el-button>
+        <el-button type="success" @click="goToJvm2008">jvm2008</el-button>
+        <el-button type="success" @click="goToCpu2016">cpu2016</el-button>
+        <el-button type="success" @click="goToCpu2017">cpu2017</el-button>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
-<script>
-// import Header from "./TableHeader.vue";
-import Left from "./TableLeft.vue";
-import Base from "./BaseData.vue";
-import Compar from "./ComparData.vue";
-import Footer from "./TableHeader.vue";
 
+<script>
+import axios from 'axios'
 export default {
-  components: {
-    // Header,
-    Left,
-    Base,
-    Compar,
-    Footer
-  },
   data() {
     return {
-      dataName: this.$route.name,
-      baseId: this.$route.params.baseId,
-      titleIndex: 1,
-      comparsionIds: this.$route.params.comparsionIds.split(","),
-
-      otherList: [],
-      tableTitle: [],
-      tableDatas: [],
-      // baseDatas:[],
-      comparDatas: {}, // 初始化 comparDatas 对象
-
-      isAllDataLoaded: false, // 标记数据是否加载完成
-      isComparDataLoaded: {}, // 标记上一个compar数据是否加载完成
-    };
+      localShowAllData: this.showAllData,
+      allProjectDatas:[],
+      isDataLoaded:false,
+    }
+  },
+  props: {
+    tableDatas: Array,    // 接收 table-datas 属性
+    dataName: String,
+    showAllData: null
   },
   created() {
-    this.isAllDataLoaded = true;
-    this.isComparDataLoaded = Array(this.comparsionIds.length + 1).fill(false)
+    axios.get('/api/project/').then((response) => {
+      this.allProjectDatas = response.data.data
+      console.log(this.allProjectDatas);
+      this.isDataLoaded = true;
+    });
+  },
+  computed: {
+    toStream() {
+      if (!this.isDataLoaded) {
+      return false; // 数据尚未加载完成，返回默认值
+    }
+      const baseData = this.allProjectDatas.find(data => data.id === parseInt(this.$route.params.baseId));
+      const comparsionIdsArray = this.$route.params.comparsionIds.split(',');
+      let disabled = false
+      if (baseData && baseData.stream === 0) {
+        disabled = true
+      }
+      comparsionIdsArray.some(compId => {
+        const comparData = this.allProjectDatas.find(data => data.env_id === parseInt(compId));
+        if (comparData && comparData.stream === 0){
+          disabled = true
+        }
+      });
+      return disabled;
+    }
   },
   methods: {
-    getHeadrData(value) {
-      // const headerComponent = this.$refs.headerComponent; // 获取 Header 子组件的引用
-      // this.otherList = headerComponent.otherList; // 访问 Header 子组件的 otherList 数据
-
-      this.otherList = value; // 访问 Header 子组件的 otherList 数据
-      // 在这里处理子组件的数据
-
+    goToStream(){
+      this.$router.push({name: 'stream',"params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
     },
-    getLeftData(value) {
-      this.tableTitle = value; // 访问 Left 子组件的 tableTitle 数据
-      // 在这里处理子组件的数据
+    goToLmbench(){
+      this.$router.push({name: 'lmbench',"params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
     },
-    getBaseData(index, value) {
-      this.titleIndex = index - 1
-      console.log(this.isComparDataLoaded, 999);
-      console.log(this.titleIndex, 888);
-      this.tableDatas = value; // 访问 base 子组件的 tableDatas 数据
-      this.isComparDataLoaded[0] = true
-      // console.log(this.tableDatas, 333);
-      // 在这里处理子组件的数据
-
-      // const comparComponents = this.$refs.comparComponent; // 获取 base 子组件的引用
-      // if (Array.isArray(comparComponents)) {
-      //   // 遍历 comparComponents 数组获取每个 Compar 组件的数据
-      //   comparComponents.forEach((comparComponent, index) => {
-      //     const comparsionId = this.comparsionIds[index];
-      //     if (comparComponent) {
-      //       this.comparDatas[comparsionId] = comparComponent.comparDatas; // 访问 Compar 子组件的 comparDatas 数据
-      //       console.log(comparComponent.comparDatas, 444);
-      //     }
-      //   });
-      // }
-
-      // const baseDatas = [
-      //   {stream1: '1', stream2: '2'},
-      //   {'stream1-1': '1-1', 'stream2-2': '2-2'}
-      // ];
-      // 遍历列表 tableDatas，并将其属性合并到列表 baseDatas(目前没有baseDatas数据，模拟一下) 中对应位置的对象,后期还要加入compar数据。
-      // this.baseDatas.forEach((bObj, index) => {
-      //   const aObj = this.tableDatas[index];
-      //   Object.assign(aObj, bObj);
-      // });
+    goToUnixbench(){
+      this.$router.push({name: 'unixbench', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
     },
-    getComparData(baseId, titleIndex_, comparDatas) {
-      this.comparDatas[baseId] = comparDatas;
+    goToFio() {
+      this.$router.push({name: 'fio', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
+    },
+    goToIozone() {
+      this.$router.push({name: 'iozone', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
+    },
+    goToJvm2008() {
+      this.$router.push({name: 'jvm2008', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
+    },
+    goToCpu2016() {
+      this.$router.push({name: 'cpu2006', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
+    },
+    goToCpu2017() {
+      this.$router.push({name: 'cpu2017', "params": {baseId: this.$route.params.baseId, comparsionIds: this.$route.params.comparsionIds}})
+    },
+    toggleDataVisibility() {
+      this.localShowAllData = !this.localShowAllData;
+      this.$emit('data-loaded', this.localShowAllData);
+    },
+    // 导出表格数据为 CSV 格式
+    exportTableData() {
+      const data = [this.tableDatas];
+
+      // 生成 CSV 格式的数据字符串
+      const csvData = data.map(rows => {
+        return rows.map(row => {
+          return Object.values(row).map(value => `"${value}"`).join(",");
+        }).join("\n");
+      }).join("\n");
+
+      // 创建并下载 CSV 文件
+      const blob = new Blob(["\uFEFF" + csvData], {type: "text/csv;charset=utf-8;"});
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = this.dataName + '_table';
+      link.click();
     },
   }
-};
+}
 </script>
-
-<style>
-@import url("//unpkg.com/element-ui@2.15.13/lib/theme-chalk/index.css");
-</style>
