93b5acdfc146bb6d1b42d4589119809bbdcc5522
wqz <wangqingzheng@kylinos.cn>
2024-08-14 16:31:54 +0800

    修改cpu2017表格样式

--- templates/front-project/src/views/Cpu2017Table.vue
+++ templates/front-project/src/views/Cpu2017Table.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <Header :tableDatas="tableDatas" :dataName="dataName" :showAllData="showAllData" @data-loaded="handleDataLoaded"/>
+    <Header :tableDatas="tableDatas" :dataName="dataName" :showAllData="showAllData"/>
     <div style="overflow-x: auto;">
-      <el-table :data="displayTableData" border :span-method="objectSpanMethod" style="overflow-x: auto;" :show-header="false">
+      <el-table :data="displayTableData" border :span-method="objectSpanMethod" style="overflow-x: auto;"
+                :show-header="false">
         <template v-for="(value, key,index) in tableDatas[0]" :key="key">
-          <el-table-column v-if="showAllData || !keysToHide.includes(key)" :prop="key" :width="index < 4 ? '80' : ''" align="center">
+          <el-table-column v-if="showAllData || !keysToHide.includes(key)" :prop="key" :width="index < 4 ? '80' : ''"
+                           align="center">
             <template v-slot="{ row }">
-              <span :style="getStyle(row, key)">{{ row[key] }}</span>
+              <div :class="getCellClassName(row, key)">
+                {{ row[key] }}
+              </div>
             </template>
           </el-table-column>
         </template>
@@ -14,7 +18,6 @@
     </div>
   </div>
 </template>
-
 
 <script>
 import axios from 'axios'
@@ -37,7 +40,7 @@ export default {
     };
   },
   created() {
-    axios.get('/api/'+this.dataName + '/?env_id=' + this.$route.params.baseId + '&comparsionIds=' + this.$route.params.comparsionIds).then((response) => {
+    axios.get('/api/' + this.dataName + '/?env_id=' + this.$route.params.baseId + '&comparsionIds=' + this.$route.params.comparsionIds).then((response) => {
       this.tableDatas = response.data.data;
       this.numColumns = Object.keys(this.tableDatas[0]).length;
       this.showAllData = false; // 默认显示平均数据
@@ -58,7 +61,7 @@ export default {
         modifiedTableData.forEach(row => {
           Object.entries(row).forEach(([key, value]) => {
             if (typeof value === 'string' && key.startsWith('column') && value.startsWith('平均值')) {
-              row[key] = this.dataName.charAt(0).toUpperCase() + this.dataName.slice(1) + "#" + `${count}`; // 将"平均值"替换为"Stream#"
+              row[key] = value + this.dataName.charAt(0).toUpperCase() + this.dataName.slice(1) + "#" + `${count}`; // 将"平均值"替换为"Stream#"
               count++;
             }
           });
@@ -68,31 +71,21 @@ export default {
     }
   },
   methods: {
-    handleDataLoaded(value) {
-      this.showAllData = value; // 访问 Left 子组件的 tableTitle 数据
-      // 在这里处理子组件的数据
-    },
-    getStyle(row, key) {
+    getCellClassName(row, key) {
       let value = row[key];
       if (typeof value === 'string' && value.endsWith('%')) {
         // 去除百分比符号 "%"
         value = value.replace('%', '');
-        // 将百分比转换为小数
+         // 将百分比转换为小数
         value = parseFloat(value);
         if (value >= 5) {
-          return {
-            color: 'red',
-            // backgroundColor: 'cyan'
-          };
+          return 'green-cell';
         } else if (value < -5) {
-          return {
-            color: 'green',
-            // backgroundColor: 'cyan'
-          };
+          return 'red-cell';
         }
       }
+      return '';
     },
-
     titleObjectSpanMethod({columnIndex}) {
       if (columnIndex === 0) {
         return [1, 5];
@@ -164,7 +157,16 @@ export default {
 }
 ;
 </script>
-
 <style>
-@import url("//unpkg.com/element-ui@2.15.13/lib/theme-chalk/index.css");
+.green-cell {
+  color: green;
+  background-color: greenyellow;
+  /* 其他样式属性 */
+}
+
+.red-cell {
+  color: red;
+  background-color: pink;
+  /* 其他样式属性 */
+}
 </style>
