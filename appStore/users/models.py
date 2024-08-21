fee2c3109c7d39c8e2b553538789a684ec9616e6
wqz <wangqingzheng@kylinos.cn>
2024-08-21 14:24:11 +0800

    增加用户创建功能

--- appStore/users/models.py
+++ appStore/users/models.py
@@ -17,8 +17,8 @@ class UserProfile(AbstractUser):
     用户模型类
     """
     user_phone = models.CharField(unique=True,null=True, max_length=22, verbose_name='手机号')
+    chinese_name = models.CharField(max_length=255, unique=True)
     is_superuser = models.BooleanField(default=False, verbose_name='是否是管理员')
-    is_staff = models.BooleanField(default=True, verbose_name='是否是员工')
 
     class Meta:
         db_table = 'user_profile'
