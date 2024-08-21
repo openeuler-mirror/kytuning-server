0bda989cbdcc13a5a708bd27ff72fd353999918b
wqz <wangqingzheng@kylinos.cn>
2024-08-21 10:51:48 +0800

    修改新版本对应的index.js文件

--- templates/front-project/src/router/index.js
+++ templates/front-project/src/router/index.js
@@ -15,92 +15,50 @@ const routes = [
     {
         name: 'Home',
         path: '/',
-        component: () => import('@/views/KylinHome'),
+        component: () => import('@/components/kytuningLogin'),
     },
     {
         path: '/project',
         name: 'project',
         component: () => import('@/views/ProjectTable'),
     },
-    // {
-    //     name: 'env',
-    //     path: '/env/:baseId',
-    //     component: () => import('@/views/EnvTable'),
-    //     // props: true
-    // },
     {
         name: 'env',
         path: '/env/:baseId/',
         component: () => import('@/views/EnvTable'),
         props: true,
     },
-    // {
-    //     name: 'unixbench',
-    //     path: '/unixbench/:baseId',
-    //     component: () => import('@/views/UnixbenchTable'),
-    // },
     {
         name: 'unixbench',
         path: '/unixbench/:baseId/:comparsionIds?',
         component: () => import('@/views/UnxibenchTable'),
         props: true,
     },
-    // {
-    //     name: 'stream',
-    //     path: '/stream/:baseId',
-    //     component: () => import('@/views/StreamTable'),
-    // },
-
     {
         name: 'stream',
         path: '/stream/:baseId/:comparsionIds?',
         component: () => import('@/views/StreamTable'),
     },
-    // {
-    //     name: 'lmbench',
-    //     path: '/lmbench/:baseId',
-    //     component: () => import('@/views/LmbenchTable'),
-    // },
     {
         name: 'lmbench',
         path: '/lmbench/:baseId/:comparsionIds?',
         component: () => import('@/views/LmbenchTable'),
     },
-    // {
-    //     name: 'fio',
-    //     path: '/fio/:baseId',
-    //     component: () => import('@/views/FioTable'),
-    // },
     {
         name: 'fio',
         path: '/fio/:baseId/:comparsionIds?',
         component: () => import('@/views/FioTable'),
     },
-    // {
-    //     name: 'iozone',
-    //     path: '/iozone/:baseId',
-    //     component: () => import('@/views/IozoneTable'),
-    // },
     {
         name: 'iozone',
         path: '/iozone/:baseId/:comparsionIds?',
         component: () => import('@/views/IozoneTable'),
     },
-    // {
-    //     name: 'jvm2008',
-    //     path: '/jvm2008/:baseId',
-    //     component: () => import('@/views/Jvm2008sTable'),
-    // },
     {
         name: 'jvm2008',
         path: '/jvm2008/:baseId/:comparsionIds?',
         component: () => import('@/views/Jvm2008Table'),
     },
-    // {
-    //     name: 'cpu2006',
-    //     path: '/cpu2006/:baseId',
-    //     component: () => import('@/views/old-view/Cpu2006Table'),
-    // },
     {
         name: 'cpu2006',
         path: '/cpu2006/:baseId/:comparsionIds?',
