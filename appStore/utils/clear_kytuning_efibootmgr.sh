822029f3ad2634980394a515a4ba57adb4da8d90
qz_cx <wangqingzheng@kylinos.cn>
2025-10-20 13:45:02 +0800

    Modify automation installation script

--- appStore/utils/clear_kytuning_efibootmgr.sh
+++ appStore/utils/clear_kytuning_efibootmgr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir wqz
+# clear_kytuning_efibootmgr.sh
+echo "=========运行clear_kytuning_efibootmgr.sh脚本========"
 # 获取所有包含 "Kytuning" 的引导项的引导号
-boot_entries=$(sudo efibootmgr | awk '/Kytuning/ {print $1}')
+boot_entries=$(efibootmgr | awk '/Kytuning/ {print $1}')
 
 if [ -n "$boot_entries" ]; then
   # 删除每个引导项
@@ -10,7 +11,7 @@ if [ -n "$boot_entries" ]; then
     clean_entry=$(echo "$entry" | tr -cd '[:alnum:]' | sed 's/Boot//')
 
     # 删除引导项
-    sudo efibootmgr -b "$clean_entry" -B
+    efibootmgr -b "$clean_entry" -B
     echo "已成功删除引导项 '$clean_entry'"
   done
 fi
