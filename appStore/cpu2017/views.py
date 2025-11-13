fea1f3c986c9c619f0385e1e2eb11238aad33675
qz_cx <wangqingzheng@kylinos.cn>
2025-11-13 14:01:49 +0800

    CPU2017 comparative data analysis backend code logic completion

--- appStore/cpu2017/views.py
+++ appStore/cpu2017/views.py
@@ -12,7 +12,7 @@ from rest_framework import status, viewsets
 from appStore.cpu2017.models import Cpu2017
 from appStore.cpu2017.serializers import Cpu2017Serializer
 from appStore.project.models import Project
-from appStore.utils.common import json_response, get_error_message
+from appStore.utils.common import json_response, get_error_message, get_analyze_data
 
 log = logging.getLogger('kytuninglog')
 
@@ -747,6 +747,8 @@ class Cpu2017ViewSet(viewsets.ModelViewSet):
             for comparativeId in comparsionIds:
                 comparsion_queryset = Cpu2017.objects.filter(env_id=comparativeId).all()
                 datas, title_index, column_index, base_column_index = self.get_data(comparsion_queryset, datas, title_index, column_index, base_column_index)
+        analyze_data = get_analyze_data(datas, 'cpu2017')
+        all_datas = {'datas': datas, 'analyze_data': analyze_data}
         return json_response(datas, status.HTTP_200_OK, '列表')
 
     def create(self, request, *args, **kwargs):
